262003fc67b2addaab92eeb5c3d393ef344fab87
abhhi <birdawade11@gmail.com>
2021-04-06 20:29:27 +0530

    request to fetch

---
+++ legit.py
@@ -0,0 +1,10 @@
+GET /scan/web.whatsapp.com/ HTTP/1.1
+Host: www.urlvoid.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Origin: https://www.urlvoid.com
+Connection: close
+Referer: https://www.urlvoid.com/
+Upgrade-Insecure-Requests: 1
